63192141592eb7b278867e116fd150c74513cbb0
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-04-03 16:51:34 -0600

    Move client to its own repo

---
+++ README.md
@@ -0,0 +1,8 @@
+## HEIC Converter
+
+### Overview
+
+Contains the logic for converting images from .heic to .jpeg
+
+Note: This is currently being used by
+[SwiftConvert](https://github.com/IM-Deane/swift-convert)
--- client/next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	staticPageGenerationTimeout: 300,
-	images: {
-		domains: ["tailwindui.com", "images.unsplash.com"],
-		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-	},
-};
